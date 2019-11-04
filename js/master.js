fd02a0c3278ebbe441530eadd6e15aef8f6546ea
李毅 <thomas@23-6.site>
2019-11-04 16:04:13 +0900

    添加输入框自动聚焦

--- js/master.js
+++ js/master.js
@@ -1,20 +1,25 @@
-// 关键字文本框
-$('#keyword').on('keydown', function() {
-  if (event.keyCode == 13) {
-    // 回车搜索
-    $("#search").click();
-  }
-});
+// 初始化
+$(function () {
 
-// 搜索按钮
-$('#search').on('click', function() {
-  if ($('#keyword').val()) {
-    // 搜索关键字
-    var url = 'https://www.baidu.com/s?word=';
-    url += $('#keyword').val();
-    location.href = url;
-  } else {
-    // 空关键字刷新
-    location.reload();
-  }
+  // 关键字文本框
+  $('#keyword').on('keydown', function() {
+    if (event.keyCode == 13) {
+      // 回车搜索
+      $("#search").click();
+    }
+  });
+
+  // 搜索按钮
+  $('#search').on('click', function() {
+    if ($('#keyword').val()) {
+      // 搜索关键字
+      var url = 'https://www.baidu.com/s?word=';
+      url += $('#keyword').val();
+      location.href = url;
+    } else {
+      // 空关键字刷新
+      location.reload();
+    }
+  });
+  
 });
