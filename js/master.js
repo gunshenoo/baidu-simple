16d391eee4906b4e95c56517f24ede5f74d00eee
李毅 <thomas@23-6.site>
2019-11-04 15:50:15 +0900

    添加 README.md

---
+++ js/master.js
@@ -0,0 +1,20 @@
+// 关键字文本框
+$('#keyword').on('keydown', function() {
+  if (event.keyCode == 13) {
+    // 回车搜索
+    $("#search").click();
+  }
+});
+
+// 搜索按钮
+$('#search').on('click', function() {
+  if ($('#keyword').val()) {
+    // 搜索关键字
+    var url = 'https://www.baidu.com/s?word=';
+    url += $('#keyword').val();
+    location.href = url;
+  } else {
+    // 空关键字刷新
+    location.reload();
+  }
+});
